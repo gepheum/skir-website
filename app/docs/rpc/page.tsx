f00137343882d742789b5cbec2ffd873661c0883
Tyler Fibonacci <gepheum@gmail.com>
2026-02-07 10:25:03 -0500

    format

--- app/docs/rpc/page.tsx
+++ app/docs/rpc/page.tsx
@@ -14,7 +14,8 @@ export default function RpcPage() {
         issue RPCs with end-to-end type safety.
       </p>
       <p>
-        Skir services are versatile: they can be used either for communication between microservices or between your frontend and your backend.
+        Skir services are versatile: they can be used either for communication between microservices
+        or between your frontend and your backend.
       </p>
 
       <h2>Core concepts</h2>
