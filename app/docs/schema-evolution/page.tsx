443e2e2434036b18a3fd84d29597693d965fa6f4
Tyler Fibonacci <gepheum@gmail.com>
2026-02-19 15:36:09 -0500

    update website

--- app/docs/schema-evolution/page.tsx
+++ app/docs/schema-evolution/page.tsx
@@ -298,7 +298,7 @@ assert(result.subscriptionStatus.union.kind === "TRIAL");  // Preserved!`}</Code
         </p>
       </Note>
 
-      <NextPageLink title="Skir services" href="/docs/services" />
+      <NextPageLink title="Typesafe RPCs" href="/docs/services" />
     </Prose>
   )
 }
